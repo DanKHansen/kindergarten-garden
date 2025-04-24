fd49e5bfcca98a1e677674f4b2a99117f2466af5
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 13:25:35 +0200

    fewer lines

--- src/main/scala/Garden.scala
+++ src/main/scala/Garden.scala
@@ -2,11 +2,10 @@ enum Plant:
    case Grass, Clover, Radishes, Violets
 
 case class Garden(s: String):
-   def plants(name: String): List[Plant] =
-      val rows = s.split("\n").map(_.sliding(2, 2).toList)
+   def plants(name: String): Seq[Plant] =
+      val rows = s.split("\n").map(_.sliding(2, 2).toSeq)
       (rows(0)(name.head - 'A') ++ rows(1)(name.head - 'A'))
          .flatMap(c => Plant.values.find(_.toString.head == c))
-         .toList
 
 object Garden:
    def defaultGarden(encoding: String): Garden = Garden(encoding)

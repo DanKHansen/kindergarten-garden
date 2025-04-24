fb747e98e479a04e75e59a5dff34aa7b8f3a88e6
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 13:08:49 +0200

    solution

--- src/main/scala/Garden.scala
+++ src/main/scala/Garden.scala
@@ -4,7 +4,8 @@ enum Plant:
 object Garden:
    def defaultGarden(encoding: String): Garden = Garden(encoding)
 
-   case class Garden(enc: String):
-      def plants(name: String): List[Plant] = ???
-
-
+case class Garden(s: String):
+      def plants(name: String): List[Plant] =
+         val (firstRow, secondRow) = s.split("\n") match
+            case Array(h, t) => (h.sliding(2, 2).toList, t.sliding(2, 2).toList)
+         (firstRow(name.head - 'A') ++ secondRow(name.head - 'A')).map(c => Plant.values.find(_.toString.head == c).get).toList

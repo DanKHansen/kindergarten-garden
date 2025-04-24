dc98a82523816204645a7c74d0faf43d16271de0
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 13:13:51 +0200

    refactor

--- src/main/scala/Garden.scala
+++ src/main/scala/Garden.scala
@@ -1,11 +1,12 @@
 enum Plant:
    case Grass, Clover, Radishes, Violets
 
+case class Garden(s: String):
+   def plants(name: String): List[Plant] =
+      val Array(firstRow, secondRow) = s.split("\n").map(_.sliding(2, 2).toList)
+      (firstRow(name.head - 'A') ++ secondRow(name.head - 'A'))
+         .flatMap(c => Plant.values.find(_.toString.head == c))
+         .toList
+
 object Garden:
    def defaultGarden(encoding: String): Garden = Garden(encoding)
-
-case class Garden(s: String):
-      def plants(name: String): List[Plant] =
-         val (firstRow, secondRow) = s.split("\n") match
-            case Array(h, t) => (h.sliding(2, 2).toList, t.sliding(2, 2).toList)
-         (firstRow(name.head - 'A') ++ secondRow(name.head - 'A')).map(c => Plant.values.find(_.toString.head == c).get).toList
